5646e38274a1f9b77f6bca9794c797f86ae59326
BHAVYA TYAGI <72131872+Bhavya1912@users.noreply.github.com>
2022-01-17 22:33:32 +0530

    Create java1.java

---
+++ java1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+         Scanner sc = new Scanner(System.in);
+        int b = scan.nextInt();
+        Scanner sa = new Scanner(System.in);
+        int c = scan.nextInt();
+        // Complete this line
+
+        System.out.println(a);
+         System.out.println(b);
+          System.out.println(c);
+        // Complete this line
+        // Complete this line
+    }
+}
